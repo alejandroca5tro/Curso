1b929f616f4d403802a6a9ef6e7c2e4106d8ef69
Alejandro Castro <alejandrocastro@gmx.es>
2017-05-30 14:48:09 +0200

    práctica

---
+++ 03_JSweb/_tareas/Practica_Unidad_2/scripts/start.js
@@ -0,0 +1,6 @@
+    var aCourses = ['Programador Web', 'Programador de Sistemas', 'Programador de Objetos'];
+    var oSubjects = {
+        'Programador Web': ['Html', 'CSS', 'JavaScript', 'PHP'],
+        'Programador de Sistemas': ['PS1', 'PS2', 'PS3', 'PS4'],
+        'Programador de Objetos': ['PO1', 'PO2', 'PO3', 'PO4']
+    };
